8216a1526864638735c9da40012e6e9c47fa55be
AeroKingdom <50646179+AeroKingdom@users.noreply.github.com>
2019-11-25 19:01:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var querystring = require('querystring');
 var express = require('express');
 var unblocker = require('unblocker');
 var Transform = require('stream').Transform;
-const frameguard = require('frameguard');
+var cors = require('cors')
 
 var app = express();
 
@@ -60,7 +60,7 @@ var unblockerConfig = {
     ]
 };
 
-app.set('X-Frame-Options', 'GHOSTSX')
+app.use(cors())
 
 // this line must appear before any express.static calls (or anything else that sends responses)
 app.use(unblocker(unblockerConfig));
